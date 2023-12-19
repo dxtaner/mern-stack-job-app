3e69eb85edc302f2017e40bc9222a63cd9467628
taner <tanerozer16@gmail.com>
2023-12-19 17:17:00 +0300

    CustomAPIError added

---
+++ errors/custom-api.js
@@ -0,0 +1,7 @@
+export default class CustomAPIError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name;
+  }
+}
